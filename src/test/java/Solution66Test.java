78186b544bb1b1385a429c4aa0e3e9ca3d2c2299
Jessie0914 <894673968@qq.com>
2020-01-24 14:35:03 +0800

    update

---
+++ src/test/java/Solution66Test.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution66Test {
+
+    @Test
+    public void movingCount() {
+        Solution66 solution66 = new Solution66();
+        int i = solution66.movingCount(10, 1, 100);
+        System.out.println(i);
+    }
+}
---
+++ src/test/java/Solution67Test.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution67Test {
+
+    @Test
+    public void cutRope() {
+        Solution67 solution67 = new Solution67();
+        int i = solution67.cutRope(8);
+        System.out.println(i);
+    }
+}
